03439a8bc3fe86c0a158fe58cec3b25ce1b7d8cb
Ash Furrow <ash@ashfurrow.com>
2013-10-13 12:54:42 -0400

    Initial Commit

---
+++ FRP/FRPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FRPAppDelegate.h
+//  FRP
+//
+//  Created by Ash Furrow on 10/13/2013.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FRP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FRP
+//
+//  Created by Ash Furrow on 10/13/2013.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRPAppDelegate class]));
+    }
+}
