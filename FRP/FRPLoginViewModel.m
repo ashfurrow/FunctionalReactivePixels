f184e9fa34fa43e62befaa879ec46b6937b1217e
Ash Furrow <ash@ashfurrow.com>
2013-10-22 20:29:02 -0400

    Broke reference cycle in FRPLoginViewModel.

--- FRP/FRPLoginViewModel.m
+++ FRP/FRPLoginViewModel.m
@@ -21,9 +21,11 @@
     self = [super init];
     if (!self) return nil;
     
+    @weakify(self);
     self.loginCommand = [[RACCommand alloc] initWithEnabled:[RACSignal combineLatest:@[RACObserve(self, username), RACObserve(self, password)] reduce:^id(NSString *username, NSString *password){
         return @(username.length > 0 && password.length > 0);
     }] signalBlock:^RACSignal *(id input) {
+        @strongify(self);
         return [FRPPhotoImporter logInWithUsername:self.username password:self.password];
     }];
     
