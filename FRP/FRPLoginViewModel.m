ab90c72d9ed13285f759663adbf51b5dbb89451a
Phat Le <ltanphat@gmail.com>
2014-02-24 15:05:13 +0700

    Add LoginViewModel test

--- FRP/FRPLoginViewModel.m
+++ FRP/FRPLoginViewModel.m
@@ -22,14 +22,20 @@
     if (!self) return nil;
     
     @weakify(self);
-    self.loginCommand = [[RACCommand alloc] initWithEnabled:[RACSignal combineLatest:@[RACObserve(self, username), RACObserve(self, password)] reduce:^id(NSString *username, NSString *password){
-        return @(username.length > 0 && password.length > 0);
-    }] signalBlock:^RACSignal *(id input) {
+    self.loginCommand = [[RACCommand alloc] initWithEnabled:[self validateLoginInputs]
+                                                signalBlock:^RACSignal *(id input) {
         @strongify(self);
         return [FRPPhotoImporter logInWithUsername:self.username password:self.password];
     }];
     
     return self;
+}
+
+- (RACSignal *)validateLoginInputs
+{
+    return [RACSignal combineLatest:@[RACObserve(self, username), RACObserve(self, password)] reduce:^id(NSString *username, NSString *password){
+        return @(username.length > 0 && password.length > 0);
+    }];
 }
 
 @end
---
+++ FRPTests/FRPPhotoImporterTests.m
@@ -0,0 +1,9 @@
+//
+//  FRPPhotoImporterTests.m
+//  FRP
+//
+//  Created by Phat, Le Tan on 2/24/14.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#include <stdio.h>
